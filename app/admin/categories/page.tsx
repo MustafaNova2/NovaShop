9d7077a4029f979b35c50539d8df379c6e0aec76
MustafaNova2 <me6499864@gmail.com>
2026-02-18 17:07:09 +0100

    feat: implement category and product management with Prisma and CRUD
    - Set up Prisma and initialized database schema
    - Implemented category groups and categories with full CRUD operations
    - Added admin product HTML interface
    - Integrated Zod for data validation
    - Implemented category grouping and default group logic
    - Added category edit, delete, and sorting features
    - Refactored database structure and folder organization
    - Improved data fetching and merging CRUD logic
    - Fixed UI refresh and validation issues

--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -1,35 +1,52 @@
 "use client";
 import styles from "./adminCategories.module.scss";
 
-import Button from "@/components/UI/button";
-import { CategoriesData } from "@/data/categories";
-import CatRow from "./_components/row";
+import CatGroupRow from "./_components/rowGroup";
+import { TReadGroup, getAllGroups } from "@/actions/category/categoryGroup";
+import AddCategoryGroup from "@/components/admin/category/addCategoryGroup";
+import { useEffect, useState } from "react";
 
 const AdminCategories = () => {
+  const [allGroups, setAllGroup] = useState<TReadGroup[]>([]);
+
+  const getData = async () => {
+    const data = await getAllGroups();
+    if (data.res) setAllGroup(data.res);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className={styles.categoryList}>
       <div className={styles.head}>
-        <h3>Add new category:</h3>
-        <Button text="Add" onClick={() => console.log("Add Cat")} />
+        <h3>Add:</h3>
+        <AddCategoryGroup onReset={getData} />
       </div>
       <div className={styles.dataTable}>
-        {CategoriesData.map((item, index) => (
+        {allGroups.length > 0 &&
+          allGroups.map((group) => (
+            <div className={styles.catLevel1} key={group.id}>
+              <CatGroupRow onReset={getData} data={group} />
+            </div>
+          ))}
+        {/* {CategoriesData.map((item, index) => (
           <div className={styles.catLevel1} key={index}>
-            <CatRow text={item.name} />
+            <CatRow name={item.name} />
 
             {item.subCategories?.map((item2, index2) => (
               <div className={styles.catLevel2} key={index2}>
-                <CatRow text={item2.name} />
+                <CatRow name={item2.name} />
 
                 {item2.subCategories?.map((item3, index3) => (
                   <div className={styles.catLevel3} key={index3}>
-                    <CatRow text={item3.name} />
+                    <CatRow name={item3.name} />
                   </div>
                 ))}
               </div>
             ))}
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
